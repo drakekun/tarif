00a19f7d378bf1fc2a76c40567ff1682cb1e1711
drakekun <drakecik@gmail.com>
2014-02-15 20:21:55 +0000

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tarif::Application.config.secret_key_base = '7532d6bcc21390fc2e687de7fec559b00b8f3b8685925d589a6882cdbd50cd08f9e5dd78a34c5eb02bfd6e3b8d2173f83b100ab88e7b600b1b212d160b6a51ea'
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get 404" do
+    get :404
+    assert_response :success
+  end
+
+end
